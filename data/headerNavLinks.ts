cc9254b8fb8b05ec0fc0246b2a9f99c7f3e35cdd
Maanav Rao <58347499+maarao@users.noreply.github.com>
2024-01-30 00:51:44 -0500

    Made the header more in-line with the vision

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,9 +1,19 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
+  { href: '/services', title: 'Services' },
   { href: '/blog', title: 'Blog' },
-  { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
+  { href: '/contact-us', title: 'Contact Us' },
   { href: '/about', title: 'About' },
+  // { href: '/projects', title: 'Projects' },
+
+  // { href: '/', title: 'Home' },
+  // { href: '/services', title: 'Investment Management' },
+  // { href: '/services', title: 'Financial Planning' },
+  // { href: '/blog', title: 'Latest Blog' },
+  // { href: '/blog', title: 'Featured Blog' },
+  // { href: '/blog', title: 'Classics' },
+  // { href: '/contact-us', title: 'Contact Us' },
+  // { href: '/about', title: 'About' },
 ]
 
 export default headerNavLinks
